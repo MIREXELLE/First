45a88ad6b44bdc266b8ab94b928aeab02cd930ae
Mirexelle <168167773+MIREXELLE@users.noreply.github.com>
2025-09-04 07:12:28 +0000

    commit

---
+++ Activities/remainder.java
@@ -0,0 +1,10 @@
+package Activities;
+
+public class remainder {
+    public static void main(String[] args) {
+        int i = 10;
+        int n = ++i%5;
+        System.out.println(n + " " + i);
+
+    }
+}
